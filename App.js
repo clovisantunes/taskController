c4a6941329bff36b5be1eab9a302166809b5ecc3
Clovis antunes <clovissantanna@gmail.com>
2023-06-05 14:55:30 -0300

    criando butoes para treino

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { useState } from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function App(){
+
+  const [ nome, setNome] = useState('anonimo')
+
+   
+function handleMudarNome(){
+  setNome('Clovis Antunes')
+}
+
   return(
-    <View style={{ flex:1, backgroundColor:'#121212', paddingTop:28 }}>
-      <Text syle={{fontSize: 28}}>Clovis</Text>
-      <Text>Treino React.Native</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{nome}</Text>  
+      <TouchableOpacity style={styles.button} onPress={handleMudarNome}>
+        <Text style={styles.buttonText}>Mudar Nome</Text>  
+      </TouchableOpacity>   
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1, 
+    backgroundColor:'#f1f1f1', 
+    paddingTop:28
+  },
+  title:{
+    fontSize:32,
+    color:'#121212',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button:{
+    backgroundColor:'blue',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 35,
+  },
+  buttonText:{
+    color:'#fff',
+    fontWeight: 'bold',
+  }
+  
+})
